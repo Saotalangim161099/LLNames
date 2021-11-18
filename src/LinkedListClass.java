dcd2fa9703c5d90ec1dfe9cb743c19181cf797c4
toankrw <toanle@kryptowire.com>
2021-11-18 13:55:56 -0500

    feat: add addName method

--- src/LinkedListClass.java
+++ src/LinkedListClass.java
@@ -1,103 +1,93 @@
 public class LinkedListClass {
-    public LLNode<String> head;
+    public LLNode head;
 
     public LinkedListClass() {
         head = null;
     }
 
-    ;
-/*
-    //lack case Node<head
-    public void addNames(String name) {
-        LLNode<String> temp = new LLNode<>(name);
-        LLNode<String> current = head;
-        LLNode<String> back = null;
-        boolean found = false;
+    public void addNode(LLNode node) {
         if (head == null) {
-            head = temp;
+            //Add letter category Node, set link
+            LLNode categoryNode = new LLNode(node.getName().charAt(0) + "", null);
+            categoryNode.setLink(node);
+            head = categoryNode;
         } else {
-            while (current != null && !found) {
-                if (((temp.getInfo()).compareTo(current.getInfo())) < 0) {
-                    back.setLink(temp);
-                    found = true;
-                } else {
-                    back = current;
-                    current = current.getLink();
-                }
+            //iterate the linked list to find when comparisionTo resulting in == -1
+            LLNode cursor = head;
+            if (node.aheadOfNode(cursor)) {
+                LLNode categoryNode = new LLNode(node.getName().charAt(0) + "", null);
+                categoryNode.setLink(node);
+                node.setLink(cursor);
+                head = categoryNode;
+                return;
             }
-            LLNode<String> tempNode = current;
-            if (found) {
-                LLNode<String> letterNode = new LLNode<>(String.valueOf(name.charAt(0)));
-                back.setLink(letterNode);
-                letterNode.setLink(temp);
-                temp.setLink(tempNode);
-            } else {
-                back.setLink(temp);
-                temp.setLink(tempNode);
-            }
-        }
-    }*/
 
-    public void addNode(LLNode<String> node) {
-        if (head == null) {
-            head = node;
-            return;
-        } else {//head is not null
-            if ((node.getName().compareTo(head.getName())) < 0) {   //node<head
-                node.setLink(head);
-                head = node;
-            } else {   //node>head
-                LLNode<String> currNode = head.getLink();
-                LLNode<String> prevNode = head;
-                while (currNode != null) {
-                    if ((node.getName().compareTo(currNode.getName())) < 0) {  //node<currNode
-                        LLNode<String> tempNode = currNode;
-                        prevNode.setLink(node);
-                        node.setLink(tempNode);
-                    } else { //node>=currNode
-                        prevNode = currNode;
-                        currNode = currNode.getLink();
+            boolean encoutneredCateogryNode = false;
+            while (cursor != null) {
+                LLNode next = cursor.getLink();
+                if(encoutneredCateogryNode == false) {
+                    encoutneredCateogryNode = isCategoryNodeForName(cursor, node.getName());
+                }
+                if (next == null) {
+                    if (encoutneredCateogryNode) {
+                        System.out.println("Creating Name Node");
+                        node.setLink(next);
+                        cursor.setLink(node);
+                    } else {
+                        System.out.println("Creating Category Node and Name Node");
+                        LLNode categoryNode = new LLNode(node.getName().charAt(0) + "", null);
+                        categoryNode.setLink(node);
+                        cursor.setLink(categoryNode);
+                        node.setLink(next);
                     }
+                    return;
+                } else if (node.aheadOfNode(next)) {
+                    if (encoutneredCateogryNode) {
+                        System.out.println("Creating Name Node 2");
+                        node.setLink(next);
+                        cursor.setLink(node);
+                    } else {
+                        System.out.println("Creating Category Node and Name Node 2");
+                        LLNode categoryNode = new LLNode(node.getName().charAt(0) + "", null);
+                        categoryNode.setLink(node);
+                        cursor.setLink(categoryNode);
+                        node.setLink(next);
+                    }
+                    return;
                 }
-                prevNode.setLink(node);
-
+                cursor = cursor.getLink();
             }
+            //Have an boolean keeping track if encountering the letter Node
+            //if next is null, add the category Node (if haven't encountered letter Node) and name Node at the end
+            //if != -1 && already encountered the letter Node ==> add the name Node, if not encountered the letter Node ==> add the letter Node and the name Node
         }
 
     }
 
-    public boolean checkForLetter(String str) { //not meaning isLetter or not, but finding the section for the current node
-        if (head == null) {
-            return false;
-        } else {
-            boolean letterPresent = false;
-            LLNode<String> currNode = head;
-            while (currNode != null && !letterPresent) {
-                if ((currNode.isLetterNode()) && str.compareTo(currNode.getName()) == 0) {
-                    letterPresent = true;
-                } else {
-                    currNode = currNode.getLink();
-                }
-            }
-            return letterPresent;
-
-        }
+    private boolean isCategoryNodeForName(LLNode node, String name) {
+        boolean test = node.getName().length() == 1 && node.getName().toLowerCase().charAt(0) == name.toLowerCase().charAt(0);
+        System.out.println("Checking Node " + node.getName() + " - " + name + " - " + test);
+        return test;
     }
+
 
     public void add(String name) {
-        LLNode<String> newNode = new LLNode<>(name, null, false);
-        if (checkForLetter(name.substring(0,1))) { //successfully find the section for the node
-            addNode(newNode);
-        } else { //not available section for the node
-            LLNode<String> newCharNode = new LLNode<>(name.substring(0, 1), null, true);
-            addNode(newCharNode);
-            addNode(newNode);
-        }
+        LLNode newNode = new LLNode(name, null);
+        addNode(newNode);
+    }
+    public void removeName(String name)
+    {
+
+    }
+
+    public void removeCategory(char category)
+    {
+
     }
 
     public boolean find(String name) {
         boolean foundName = false;
-        LLNode<String> currentNode = new LLNode<>(name, null, false);
+        LLNode currentNode = new LLNode(name, null);
         while (currentNode != null && !foundName) {
             if ((currentNode.getName().compareTo(name)) == 0) {
                 foundName = true;
@@ -108,25 +98,9 @@ public class LinkedListClass {
         return foundName;
 
     }
-    public void showNames(){
-        LLNode currentNode=head;
-        while (currentNode!=null){
-            if (currentNode.isLetterNode){
-                System.out.println(currentNode.getName());
-            }
-            else{
-                System.out.println(" "+currentNode.getName());
-            }
-            currentNode=currentNode.getLink();
-        }
-    }
-
-
-
-
 
     public void printList() {
-        LLNode<String> currentNode = head;
+        LLNode currentNode = head;
         while (currentNode != null) {
             System.out.println(currentNode.getName());
             currentNode = currentNode.getLink();
